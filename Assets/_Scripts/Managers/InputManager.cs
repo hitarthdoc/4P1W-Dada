973fc3a53cd4be7c883050ad455213c8ee6f0f9f
hitarthdoc <hitarthdoctor@gmail.com>
2017-01-27 00:28:41 +0530

    Formatted Input Manager and Spawner.

--- Assets/_Scripts/Managers/InputManager.cs
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
