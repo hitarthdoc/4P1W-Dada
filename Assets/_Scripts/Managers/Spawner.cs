61452ae0d920cdf9cdc80ac3111ce191fc5122c8
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-28 23:36:35 +0530

    Created functions: Spawn Current Level Object and Attach Listeners
    Created function to Spawn Current Level Object and anohter to Attach the
    Listener to the Buttons spawned with the letters as params.

--- Assets/_Scripts/Managers/Spawner.cs
+++ Assets/_Scripts/Managers/Spawner.cs
@@ -39,7 +39,43 @@ namespace Managers
 		// Update is called once per frame
 		void Update ()
 		{
-	
+			
 		}
+
+		private void SpawnCurrentLevel ()
+		{
+			ClearCurrentLevel ();
+
+			/*	TODO:
+			 * 		SEND currentLevel.Word to someOne for tracking.
+			*/
+
+			foreach ( Sprite image in CurrentLevel.Pics )
+			{
+				GameObject newImageReference = Instantiate ( ImagePrefab, ImagesHolder ) as GameObject;
+				newImageReference.GetComponent <Image> ().sprite = image;
+			}
+
+			foreach ( char optionLetter in CurrentLevel.OtherChars )
+			{
+				GameObject newOptionButtonReference = Instantiate ( OptionLetterButtonPrefab, OptionButtonsHolder ) as GameObject;
+				newOptionButtonReference.GetComponentInChildren <Text> ().text = optionLetter.ToString ();
+
+				AttachListener ( newOptionButtonReference.GetComponent <Button> (), 1, optionLetter );
+
+			}
+
+		}
+
+		private void AttachListener ( Button attachToThis, int typeOfCall, char argutmentToPass )
+		{
+			attachToThis.onClick.AddListener ( 
+				delegate
+				{
+					IPManReference.OnClickInputLetter ( argutmentToPass );
+				}
+			);
+		}
+
 	}
 }
