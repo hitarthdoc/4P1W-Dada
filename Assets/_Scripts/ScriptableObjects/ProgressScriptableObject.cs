77f9cf8b147fb62acef7442c030243f9fb63cc5e
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-28 23:21:09 +0530

    Added field declarations.
    Added the following fields for:
    LSO reference,
    Current Level,
    Current Batch,
    Current Level Index in Batch,
    and
    Level Indices to Select from Current Batch.

--- Assets/_Scripts/ScriptableObjects/ProgressScriptableObject.cs
+++ Assets/_Scripts/ScriptableObjects/ProgressScriptableObject.cs
@@ -1,10 +1,33 @@
-﻿using UnityEngine ;
-using System.Collections ;
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+using SO.Levels;
 
 namespace SO.Progress
 {
-	public class ProgressScriptableObject : MonoBehaviour
+
+	public class ProgressScriptableObject : ScriptableObject
 	{
-	
+		[SerializeField]
+		LevelScriptableObject LSO;
+
+		// Should always Initialize with "0". ONLY ON First App Launch;
+		[SerializeField]
+		int CurrentLevel = 0;
+
+		// Should always Initialize with "0". ONLY ON First App Launch;
+		[SerializeField]
+		int CurrentBatch;
+		// = CurrentLevel / 10;
+
+		[SerializeField]
+		int CurrentLevelIndexInBatch;
+
+		// Update on Batch change and initialize with First Batch
+		[SerializeField]
+		List <int> LevelIndicesToSelectFromCurrentBatch;
+
 	}
 }
