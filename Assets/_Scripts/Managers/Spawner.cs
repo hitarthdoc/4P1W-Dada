2ba80f4e969c393e73860fca808daf93a286914d
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-28 23:33:35 +0530

    Added fields in Spawner.

--- Assets/_Scripts/Managers/Spawner.cs
+++ Assets/_Scripts/Managers/Spawner.cs
@@ -7,6 +7,29 @@ namespace Managers
 	public class Spawner : MonoBehaviour
 	{
 
+		[SerializeField]
+		LevelScriptableObject LSO;
+
+		[SerializeField]
+		ProgressScriptableObject PSO;
+
+		[SerializeField]
+		InputManager IPManReference;
+
+		public GameObject OptionLetterButtonPrefab;
+
+		public GameObject ImagePrefab;
+
+		public GameObject AnswerLetterButtonPrefab;
+
+		public Transform ImagesHolder;
+
+		public Transform AnswerButtonsHolder;
+
+		public Transform OptionButtonsHolder;
+
+		private Level CurrentLevel;
+
 		// Use this for initialization
 		void Start ()
 		{
