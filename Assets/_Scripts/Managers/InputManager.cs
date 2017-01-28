122815d481e2aa6ddbd77788db099308cb210332
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-28 23:15:24 +0530

    Added OnClickInputLetter Function.
    Added OnClickInputLetter Function with Only Debug, to test.

---
+++ Assets/_Scripts/Managers/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Managers
+{
+
+	public class InputManager : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void OnClickInputLetter ( char letterPressed )
+		{
+			Debug.Log ( letterPressed );
+		}
+
+	}
+}
