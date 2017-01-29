28f9ab4b4c6e98ae25e6e0b612415b7f2d35279e
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-29 20:02:12 +0530

    Created Answer Button State Manager.

---
+++ Assets/_Scripts/AnswerButtonStateManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+namespace States.Answers
+{
+
+	public enum AnswerButtonStates
+	{
+		Lettered,
+		NotLettered,
+		Disabled,
+		Default,
+	}
+
+	public class AnswerButtonStateManager : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start ()
+		{
+		}
+
+
+	}
+
+}
