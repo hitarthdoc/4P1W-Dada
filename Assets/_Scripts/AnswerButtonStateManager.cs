6663cc814dcdcdf520ab71ec580f9d3ae8821a45
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-29 20:05:17 +0530

    Added Awake for Initialization and OnClickt to intercept the Button's OnClick events.

--- Assets/_Scripts/AnswerButtonStateManager.cs
+++ Assets/_Scripts/AnswerButtonStateManager.cs
@@ -38,11 +38,52 @@ namespace States.Answers
 		[SerializeField]
 		Text textComponent;
 
+		// Use this for initialization before Start
+		void Awake ()
+		{
+			buttonComponent = GetComponent <Button> ();
+			textComponent = GetComponentInChildren <Text> ();
+
+			buttonComponent.onClick.AddListener 
+			( 
+				delegate
+				{
+					this.OnClick ();
+				} 
+			);
+		}
+
 		// Use this for initialization
 		void Start ()
 		{
 		}
 
+		void OnClick ()
+		{
+			switch ( currentButtonState )
+			{
+
+				case AnswerButtonStates.Lettered:
+					currentButtonState = AnswerButtonStates.NotLettered;
+					ATManReference.RemoveLetterFromTypedWord ( letter, transform.GetSiblingIndex () );
+//					IPManReference.AcceptOption ( letter );
+					textComponent.text = "";
+					break;
+
+				case AnswerButtonStates.NotLettered:
+					break;
+
+				case AnswerButtonStates.Disabled:
+					break;
+
+				case AnswerButtonStates.Default:
+					break;
+
+				default:
+					Debug.Log ( "We Have a Problem" );
+					break;
+			}
+		}
 
 	}
 
