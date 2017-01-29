f38735d467c3ca11ec287b9d24c422ab89dcfb10
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-29 19:53:38 +0530

    Added Answer Text Manager (ATMan).

---
+++ Assets/_Scripts/Managers/AnswerTextManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+using States.Options;
+
+namespace Managers
+{
+
+	public class AnswerTextManager : MonoBehaviour
+			
+//			Debug.Log ( answerButtonParent.childCount );
+		}
+
+	}
+}
