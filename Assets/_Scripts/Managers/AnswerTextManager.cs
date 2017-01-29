6397e188928e03043a374fcfdfbb8bb5da42915e
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-29 19:56:33 +0530

    Created fields.
    Created fields for:
    the answer + property,
    the letters typed,
    the options buttons who typed them.

--- Assets/_Scripts/Managers/AnswerTextManager.cs
+++ Assets/_Scripts/Managers/AnswerTextManager.cs
@@ -9,6 +9,25 @@ namespace Managers
 {
 
 	public class AnswerTextManager : MonoBehaviour
+	{
+		[SerializeField]
+		string answerWord;
+
+		public string AnswerWord {
+			get	{ return answerWord; }
+			set
+			{
+				answerWord = value;
+				typedLetters = new List<char> ( answerWord.Length );
+			}
+		}
+
+		[SerializeField]
+		List <char> typedLetters = new List<char> ( 0 );
+
+		[SerializeField]
+		List<OptionButtonStateManager> optionButtonReferences;
+
 			
 //			Debug.Log ( answerButtonParent.childCount );
 		}
