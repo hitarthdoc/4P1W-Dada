d6a587bc2e8805d8230ab8a0e691c5199a79d427
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-01-29 19:47:46 +0530

    Renamed OpitonButtonState to OptionButtonState.

--- Assets/_Scripts/OpitonButtonState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class OpitonButtonState : MonoBehaviour
-{
-
-
-
-}
---
+++ Assets/_Scripts/OptionButtonStateManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+using Managers;
+
+namespace States.Options
+{
+
+	public class OptionButtonStateManager : MonoBehaviour
+	{
+	}
+}
