380c6e1985731d65f2931c464afff8373393ea72
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-02-05 20:13:04 +0530

    Added money scriptable object to handle the economy based power-up system system.

---
+++ Assets/_Scripts/ScriptableObjects/MoneyScriptableObject.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SO.Money
+{
+
+	public class MoneyScriptableObject : ScriptableObject
+	{
+
+		[SerializeField]
+		private int moneyEarned = 100;
+
+		[SerializeField]
+		private int moneyEarnedPerLevel;
+
+		[SerializeField]
+		private int moneySpentOnPowerUp_1;
+
+		[SerializeField]
+		private int moneySpentOnPowerUp_2;
+
+		public int MoneyEarned {
+			get
+			{
+				return moneyEarned;
+			}
+		}
+
+		public void IncreaseMoney (int increaseBy)
+		{
+			moneyEarned += increaseBy;
+		}
+
+		public void IncreaseMoneyOnLevelCleared ()
+		{
+			moneyEarned += moneyEarnedPerLevel;
+		}
+
+		public void DecreaseMoneyOnPowerUp_1 ()
+		{
+			moneyEarned -= moneySpentOnPowerUp_1;
+		}
+
+		public void DecreaseMoneyOnPowerUp_2 ()
+		{
+			moneyEarned -= moneySpentOnPowerUp_2;
+		}
+
+	}
+}
