686123874daefbdf794917ba5795c47c21acbdc1
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-02-05 20:41:48 +0530

    Created a custom editor for Level Scriptable Objects assets.
    Description:
    1. Displays the stats of the asset. List:
    a. Batch Count
    b. Max Batches
    c. Max Levels in Batches
    d. Max Levels Total
    e. Current Levels Total
    
    2. Can Perform Add, Remove at index and Delete at index operations with
    button interface on Batches and Levels.
    3. Easy per field Reset options created for each exposed and required
    field for Level.
    4, Created Easy GUI Layout for quick editing.
    5. Level and Level Batch objects are collapsible individually, to avoid
    clutter.

---
+++ Assets/Editor/LevelSOEditor.cs
@@ -0,0 +1,275 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+using SO.Levels;
+
+[CustomEditor ( typeof ( LevelScriptableObject ) )]
+public class LevelScriptEditor : Editor
+{
+	public override void OnInspectorGUI ()
+	{
+
+		LevelScriptableObject myTarget = ( LevelScriptableObject ) target;
+
+		Rect LSODetsAndMaxes = EditorGUILayout.BeginVertical ();
+		{
+			int totalLevels = 0;
+			foreach ( LevelBatch LB in myTarget.LevelBatches )
+			{
+				totalLevels += LB.Levels.Count;
+			}
+
+			EditorGUILayout.LabelField ( "Batch Count:\t", myTarget.LevelBatches.Capacity.ToString () );
+			EditorGUILayout.LabelField ( "Max Batches:\t", myTarget.MaxBatches.ToString () );
+			EditorGUILayout.LabelField ( "Max Levels in Batches:\t", myTarget.MaxLevelsInBatches.ToString () );
+			EditorGUILayout.LabelField ( "Max Levels Total:\t", ( myTarget.MaxLevelsInBatches * myTarget.MaxBatches ).ToString () );
+			EditorGUILayout.LabelField ( "Current Levels Total:\t", totalLevels.ToString () );
+//		EditorGUILayout.LabelField ( "Level", myTarget.LevelBatches.ToString () );
+		}
+		EditorGUILayout.EndVertical ();
+
+		if ( myTarget.LevelBatches.Count < myTarget.MaxBatches )
+		{
+			if ( GUILayout.Button ( "Add Batch" ) )
+			{
+				myTarget.AddBatch ();
+			}
+		}
+
+		Rect LSO = EditorGUILayout.BeginVertical ();
+
+		int batchIndex = 0;
+
+		foreach ( LevelBatch batch in myTarget.LevelBatches )
+		{
+			
+			Rect batchAndItsOption = EditorGUILayout.BeginHorizontal ();
+			{
+				batch.show = EditorGUILayout.Foldout ( batch.show, "Batch:\t" + batchIndex.ToString () );
+
+				if ( GUILayout.Button ( "Delete Batch" ) )
+				{
+					myTarget.DeleteBatch ( batchIndex );
+					//Debug.Log ( "Here" );
+				}
+				if ( GUILayout.Button ( "Reset Batch" ) )
+				{
+					myTarget.ResetBatch ( batchIndex );
+					//Debug.Log ( "Here" );
+				}
+
+				if ( batch.Levels.Count < myTarget.MaxLevelsInBatches )
+				{
+					if ( GUILayout.Button ( "Add Level" ) )
+					{
+						batch.AddLevel ();
+					}
+				}
+				
+				batchIndex++;
+			}
+			EditorGUILayout.EndHorizontal ();
+
+			if ( batch.show )
+			{
+				Rect batchRect = EditorGUILayout.BeginVertical ();
+				{
+//					EditorGUILayout.LabelField ( "Batch:\t" + batchIndex );
+				
+					int levelIndex = 0;
+
+					foreach ( Level level in batch.Levels )
+					{
+						
+						Rect levelAndItsOption = EditorGUILayout.BeginHorizontal ();
+						{
+							level.show = EditorGUILayout.Foldout ( level.show, "Level:\t" + levelIndex.ToString () );
+
+							if ( GUILayout.Button ( "Delete Level" ) )
+							{
+								batch.DeleteLevel ( levelIndex );
+								//Debug.Log ( "Here" );
+							}
+							if ( GUILayout.Button ( "Reset Level" ) )
+							{
+								batch.ResetLevel ( levelIndex );
+								//Debug.Log ( "Here" );
+							}
+							
+							levelIndex++;
+						}
+						EditorGUILayout.EndHorizontal ();
+
+						if ( level.show )
+						{
+							string word	= level.Word;
+							//EditorGUILayout.LabelField ( "Level:\t" + levelIndex );
+							Rect levelRect = EditorGUILayout.BeginVertical ();
+							{
+								Rect answerRectHori = EditorGUILayout.BeginHorizontal ();
+								{
+									level.Word = EditorGUILayout.TextField ( "Answer:", level.Word );
+
+									if ( !level.Word.Equals ( word ) )
+									{
+										level.ClearAnswered ();
+									
+									}
+
+									if ( GUILayout.Button ( "Clear Answer" ) )
+									{
+										level.ClearWord ();
+										//Debug.Log ( "Here" );
+									}
+								}
+								EditorGUILayout.EndHorizontal ();
+
+								EditorGUILayout.Space ();
+
+								Rect optionsRectHori = EditorGUILayout.BeginHorizontal ();
+								{
+									EditorGUILayout.LabelField ( "Options:" );
+
+									Rect optionsRectVert = EditorGUILayout.BeginVertical ();
+									{
+
+										Rect optionsRowRect_1 = EditorGUILayout.BeginHorizontal ();
+										{
+
+											for ( int i = 0; i < 6; i++ )
+											{
+												level.OtherChars [ i ] = EditorGUILayout.TextField ( level.OtherChars [ i ].ToString (), GUILayout.Width ( 20.0F ) ) [ 0 ];
+											}
+										}
+										EditorGUILayout.EndHorizontal ();
+
+										Rect optionsRowRect_2 = EditorGUILayout.BeginHorizontal ();
+										{
+											for ( int i = 6; i < 12; i++ )
+											{
+												level.OtherChars [ i ] = EditorGUILayout.TextField ( level.OtherChars [ i ].ToString (), GUILayout.Width ( 20.0F ) ) [ 0 ];
+											}
+										}
+										EditorGUILayout.EndHorizontal ();
+									}
+									EditorGUILayout.EndVertical ();
+
+									if ( GUILayout.Button ( "Clear Options" ) )
+									{
+										level.ClearOptions ();
+										//Debug.Log ( "Here" );
+									}
+								}
+								EditorGUILayout.EndHorizontal ();
+
+								EditorGUILayout.Space ();
+
+								Rect picsRectHori = EditorGUILayout.BeginHorizontal ();
+								{
+									EditorGUILayout.LabelField ( "Selected Pictures:" );
+
+									Rect addedLettersRowRect_1 = EditorGUILayout.BeginHorizontal ();
+									{
+										for ( int i = 0; i < 4; i++ )
+										{
+											level.Pics [ i ] = EditorGUILayout.ObjectField ( level.Pics [ i ], typeof ( Sprite ), false ) as Sprite;
+										}
+									}
+									EditorGUILayout.EndHorizontal ();
+
+									if ( GUILayout.Button ( "Clear Selected Pictures" ) )
+									{
+										level.ClearSelectedPictures ();
+										//										Debug.Log ( "Here" );
+									}
+								}
+								EditorGUILayout.EndHorizontal ();
+
+								EditorGUILayout.Space ();
+
+								Rect removedLettersRectHori = EditorGUILayout.BeginHorizontal ();
+								{
+									EditorGUILayout.LabelField ( "Removed Letters:" );
+
+									Rect removedLettersRectVert = EditorGUILayout.BeginVertical ();
+									{
+
+										Rect removedLettersRowRect_1 = EditorGUILayout.BeginHorizontal ();
+										{
+
+											for ( int i = 0; i < 6; i++ )
+											{
+												level.RemovedLetters [ i ] = EditorGUILayout.Toggle ( level.RemovedLetters [ i ], GUILayout.Width ( 20.0F ) );
+											}
+										}
+										EditorGUILayout.EndHorizontal ();
+
+										Rect removedLettersRowRect_2 = EditorGUILayout.BeginHorizontal ();
+										{
+											for ( int i = 6; i < 12; i++ )
+											{
+												level.RemovedLetters [ i ] = EditorGUILayout.Toggle ( level.RemovedLetters [ i ], GUILayout.Width ( 20.0F ) );
+											}
+										}
+										EditorGUILayout.EndHorizontal ();
+									}
+									EditorGUILayout.EndVertical ();
+
+									if ( GUILayout.Button ( "Clear Removed" ) )
+									{
+										level.ClearRemoved ();
+//										Debug.Log ( "Here" );
+									}
+								}
+								EditorGUILayout.EndHorizontal ();
+
+								EditorGUILayout.Space ();
+
+								Rect addedLettersRectHori = EditorGUILayout.BeginHorizontal ();
+								{
+									EditorGUILayout.LabelField ( "Added Letters:" );
+
+									Rect addedLettersRectVert = EditorGUILayout.BeginVertical ();
+									{
+
+										Rect addedLettersRowRect_1 = EditorGUILayout.BeginHorizontal ();
+										{
+
+											for ( int i = 0; i < level.AnsweredLetters.Count; i++ )
+											{
+												level.AnsweredLetters [ i ] = EditorGUILayout.Toggle ( level.AnsweredLetters [ i ], GUILayout.Width ( 20.0F ) );
+											}
+										}
+										EditorGUILayout.EndHorizontal ();
+
+									}
+									EditorGUILayout.EndVertical ();
+
+									if ( GUILayout.Button ( "Clear Answered" ) )
+									{
+										level.ClearAnswered ();
+										//										Debug.Log ( "Here" );
+									}
+								}
+								EditorGUILayout.EndHorizontal ();
+
+								EditorGUILayout.Space ();
+
+								EditorGUILayout.LabelField ( "", GUI.skin.horizontalSlider );
+							}
+							EditorGUILayout.EndVertical ();
+						}
+					}
+
+				}
+				EditorGUILayout.EndVertical ();
+			}
+//			EditorGUILayout.LabelField ( "", GUI.skin.horizontalSlider );
+		}
+
+		EditorGUILayout.EndVertical ();
+
+		EditorUtility.SetDirty ( myTarget );
+	}
+}
