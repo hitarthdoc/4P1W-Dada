518352fb6573a8e971a81fc59c2284a42dfee4b6
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-02-05 20:29:02 +0530

    Modified classes Level, LevelBatch, LevelScriptableObject.
    Level:
    1. Added default inittializtions for its fields.
    2. Changed OtherChars from char [] to List<char>
    3. Added Constructor
    4. Added Clear functions For:
    a. Word
    b. Options
    c. Removed Letters
    d. Answered Letters
    e. Selected Pictured
    5. Added public bool show for Level editor.

--- Assets/_Scripts/ScriptableObjects/LevelScriptableObject.cs
+++ Assets/_Scripts/ScriptableObjects/LevelScriptableObject.cs
@@ -9,19 +9,24 @@ namespace SO.Levels
 	[System.Serializable]
 	public class Level
 	{
+		public bool show = false;
+
 		[SerializeField]
 		private bool completionStatus = false;
 
-		public bool CompletionStatus
-		{
-			get	{	return completionStatus;	}
+		public bool CompletionStatus {
+			get	{ return completionStatus; }
 		}
 
 		public List <Sprite> Pics = new List<Sprite> ( 4 );
 
-		public 	string Word;
+		public 	string Word = "train";
 
-		public char [] OtherChars;
+		public List <bool> AnsweredLetters = new List<bool> ( 5 );
+
+		public List <bool> RemovedLetters = new List<bool> ( 12 );
+
+		public List <char> OtherChars = new List<char> ( 12 );
 
 		public void SetCompletionStatus ()
 		{
@@ -32,6 +37,74 @@ namespace SO.Levels
 		{
 			completionStatus = false;
 		}
+
+		public void ClearWord ()
+		{
+			Word = "";
+			ClearAnswered ();
+
+		}
+
+		public void ClearOptions ()
+		{
+			OtherChars.Clear ();
+
+			for ( int i = 0; i < 12; i++ )
+			{
+				OtherChars.Add ( default (char) );
+
+			}
+
+		}
+
+		public void ClearRemoved ()
+		{
+			RemovedLetters.Clear ();
+
+			for ( int i = 0; i < 12; i++ )
+			{
+				RemovedLetters.Add ( default (bool) );
+
+			}
+
+		}
+
+		public void ClearAnswered ()
+		{
+			AnsweredLetters.Clear ();
+
+			for ( int i = 0; i < Word.Length; i++ )
+			{
+				AnsweredLetters.Add ( default (bool) );
+
+			}
+
+		}
+
+		public void ClearSelectedPictures ()
+		{
+			Pics.Clear ();
+
+			for ( int i = 0; i < 12; i++ )
+			{
+				Pics.Add ( default (Sprite) );
+
+			}
+
+		}
+
+		public Level ()
+		{
+			
+			show = false;
+			completionStatus = false;
+			this.ClearWord ();
+			this.ClearOptions ();
+			this.ClearRemoved ();
+//			this.ClearAnswered ();
+			this.ClearSelectedPictures ();
+		}
+
 	}
 
 	[System.Serializable]
