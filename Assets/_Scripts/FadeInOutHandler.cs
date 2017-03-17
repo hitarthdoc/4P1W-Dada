7cf72b40adbaccf937bb27d77233c7848ed4e579
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-03-17 21:34:15 +0530

    Fade In and Out Handler.

---
+++ Assets/_Scripts/FadeInOutHandler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInOutHandler : MonoBehaviour
+{
+
+	[SerializeField]
+	Animator animRef;
+
+	[SerializeField]
+	GameObject previousPanel;
+
+	[SerializeField]
+	GameObject nextPanel;
+
+	[SerializeField]
+	float timeToTakeForAnimation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		animRef = GetComponent <Animator> ();
+	}
+
+	public void StartFadeInOutBetween ( GameObject prev, GameObject next, float TimeToTake )
+	{
+		previousPanel = prev;
+		nextPanel = next;
+		timeToTakeForAnimation = TimeToTake;
+
+		animRef.Play ( "FadeIn" );
+
+	}
+
+	public void TransitionDone ()
+	{
+		gameObject.SetActive ( false );
+	}
+
+}
