1e4ea2d1d053c2f29e1eac1b055412f95566cc7e
hitarthdoc <hitarthdoctor1994@gmail.com>
2017-03-17 21:40:17 +0530

    Added Audio Functionality and Next Level System.

--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿#undef TESTING
+
+using UnityEngine;
 using System.Collections;
 
 using UnityEngine.UI;
@@ -10,6 +12,17 @@ namespace Managers
 
 	public class UIManager : MonoBehaviour
 	{
+		const string commonButton = "common button";
+
+		const string nextLevelButton = "next level button";
+
+		const string smallImageButton = "small image button";
+
+		const string bigImageButton = "big image button";
+
+		const string levelComplete = "level complete";
+
+		const string muteToggle = "MuteToggle";
 
 		enum UIStates
 		{
@@ -38,11 +51,16 @@ namespace Managers
 
 		public AudioMixer Mixer;
 
+		public AudioManager AudioManRef;
+
+		public Toggle SoundToggleRef;
+
 		public Spawner SpawnerReference;
 
 		public delegate void StartGame ();
 
 		public event StartGame OnStartGame;
+		public event StartGame OnStartNextLevel;
 
 		UIStates current = UIStates.Default;
 		UIStates previous = UIStates.Default;
@@ -50,6 +68,29 @@ namespace Managers
 		// Use this for initialization
 		void Start ()
 		{
+
+			if ( PlayerPrefs.HasKey ( muteToggle ) )
+			{
+				if ( PlayerPrefs.GetInt ( muteToggle ) == 1 )
+				{
+					AudioManRef.ChangeMuteState ( true );
+					SoundToggleRef.isOn = false;
+				}
+
+				if ( PlayerPrefs.GetInt ( muteToggle ) == 0 )
+				{
+					AudioManRef.ChangeMuteState ( false );
+					SoundToggleRef.isOn = true;
+				}
+
+			}
+			else
+			{
+				PlayerPrefs.SetInt ( muteToggle, 0 );
+				
+				AudioManRef.ChangeMuteState ( false );
+			}
+			PlayerPrefs.Save ();
 			NoMenus ();
 			ToMainMenu ();
 			current = previous = UIStates.MainMenu;
@@ -57,11 +98,15 @@ namespace Managers
 
 		void OnEnable ()
 		{
+			AnswerTextManager.OnLevelComplete += ToLevelCompletePanel;
+
 			InputManager.OnEscapePressed += EscapePressed;
 		}
 
 		void OnDisable ()
 		{
+			AnswerTextManager.OnLevelComplete -= ToLevelCompletePanel;
+
 			InputManager.OnEscapePressed -= EscapePressed;
 		}
 
@@ -103,6 +148,9 @@ namespace Managers
 				OnStartGame ();
 			}
 
+			PlaySound ( commonButton );
+			PlaySound ( nextLevelButton );
+
 		}
 
 		public void ToExitPanel ()
@@ -111,17 +159,26 @@ namespace Managers
 			current = UIStates.ExitPanel;
 
 			MainMenu.SetActive ( false );
+
+			PlaySound ( commonButton );
+
 		}
 
-		public void OnSmallImagePressed (int selfIndex)
+		public void OnSmallImagePressed ( int selfIndex )
 		{
-			BigImageReference.sprite = SpawnerReference.GetImageatindex (selfIndex);
-			BigImagePanel.SetActive (true);
+			BigImageReference.sprite = SpawnerReference.GetImageatindex ( selfIndex );
+			BigImagePanel.SetActive ( true );
+
+			PlaySound ( smallImageButton );
+
 		}
 
 		public void OnBigImagePressed ()
 		{
-			BigImagePanel.SetActive (false);
+			BigImagePanel.SetActive ( false );
+
+			PlaySound ( bigImageButton );
+
 		}
 
 		public void ToLevelCompletePanel ()
@@ -134,6 +191,86 @@ namespace Managers
 
 			MainMenu.SetActive ( false );
 			ExitPanel.SetActive ( false );
+
+			PlaySound ( levelComplete );
+		}
+
+		public void ToNextLevel ()
+		{
+			PlayScreen.SetActive ( true );
+			NotificationPanel.SetActive ( true );
+
+			current = UIStates.PlayScreen;
+
+			LevelCompletePanel.SetActive ( false );
+
+			MainMenu.SetActive ( false );
+			ExitPanel.SetActive ( false );
+
+			if ( OnStartNextLevel != null )
+			{
+				OnStartNextLevel ();
+			}
+
+			PlaySound ( nextLevelButton );
+		}
+
+		public void SoundToggle ( bool SoundEnabled )
+		{
+			#if UNITY_EDITOR && TESTING
+			Debug.Log ( string.Format ( "mute State:\t{0}\tToggleState:\t{1}" +
+					"\nif ( !( PlayerPrefs.GetInt ( muteToggle ) == 0 ) & !SoundEnabled ) = {2}" +
+					"\nif ( !( PlayerPrefs.GetInt ( muteToggle ) == 0 ) & !SoundEnabled ) = {3}",
+				                           ( ( PlayerPrefs.GetInt ( muteToggle ) == 0 ) ? "False" : "True" ), SoundEnabled,
+				                           ( ( PlayerPrefs.GetInt ( muteToggle ) == 0 ) & !SoundEnabled ),
+				                           ( ( PlayerPrefs.GetInt ( muteToggle ) == 1 ) & SoundEnabled ) ) );
+			#endif
+
+			if ( ( PlayerPrefs.GetInt ( muteToggle ) == 0 ) & !SoundEnabled )
+			{
+				#if UNITY_EDITOR && TESTING
+//				Debug.Log ( "Here 1" );
+				#endif
+				PlayerPrefs.SetInt ( muteToggle, 1 );
+			}
+			else
+			if ( ( PlayerPrefs.GetInt ( muteToggle ) == 1 ) & SoundEnabled )
+			{
+				PlayerPrefs.SetInt ( muteToggle, 0 );
+			}
+			PlayerPrefs.Save ();
+
+			AudioManRef.ChangeMuteState ( !SoundEnabled );
+
+		}
+
+		private void PlaySound ( string soundEffectToPlay )
+		{
+			switch ( soundEffectToPlay )
+			{
+				case commonButton:
+					AudioManRef.PlayCommonUIButtonSound ();
+					break;
+
+				case levelComplete:
+					AudioManRef.PlayCorrectAnswerSound ();
+					break;
+
+				case nextLevelButton:
+					AudioManRef.PlayNextQuestionSound ();
+					break;
+
+				case smallImageButton:
+					AudioManRef.PlayHintImageInUIButtonSound ();
+					break;
+
+				case bigImageButton:
+					AudioManRef.PlayHintImageOutUIButtonSound ();
+					break;
+
+				default:
+					break;
+			}
 		}
 
 		public void EscapePressed ()
@@ -157,11 +294,15 @@ namespace Managers
 					break;
 				
 				case UIStates.Default:
+					#if UNITY_EDITOR
 					Debug.Log ( "initialization Prob" );
+					#endif
 					break;
 				
 				default:
+					#if UNITY_EDITOR
 					Debug.Log ( "Bigger Prob" );
+					#endif
 					break;
 			}
 		}
